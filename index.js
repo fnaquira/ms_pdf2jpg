cf962f0dcb3837a1e6f5ffe0405b1efa76fd00f7
Favio Náquira <fnaquiravargas@gmail.com>
2023-02-26 13:41:11 -0500

    [IMP] Funcionamiento completo

--- index.js
+++ index.js
@@ -1,37 +1,81 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const pdf2pic = require("pdf2pic");
-const fs = require("fs-extra");
+const path = require("path");
+const { fromPath } = require("pdf2pic");
+const { writeFileSync, unlink } = require("fs-extra");
+const asyncForEach = require("asyncforeach_pe");
+
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "./temp/");
+	},
+	filename: function (req, file, cb) {
+		cb(null, Date.now() + "-" + file.originalname);
+	},
+});
+
+const upload = multer({
+	storage: storage,
+	fileFilter: function (req, file, cb) {
+		if (file.mimetype !== "application/pdf") {
+			return cb(new Error("Solo se permiten archivos de PDF"));
+		}
+		cb(null, true);
+	},
+});
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
 app.use(bodyParser.json());
 
-app.post("/convertir", async (req, res) => {
-	const pdfBuffer = req.body.pdf;
-	const outputFolder = "output";
-	const options = {
+app.post("/convertir", upload.single("pdf"), async (req, res) => {
+	// Guardar el archivo PDF en la carpeta temporal
+	const pdfFile = req.file;
+	const pdfFilename = pdfFile.filename;
+	const tempFolder = "./temp";
+	const pdfPath = path.join(tempFolder, pdfFilename);
+	//return res.json({ message: "Hola mundo" });
+
+	const outputFolder = "./output";
+	const baseOptions = {
 		density: 100,
 		saveFolder: outputFolder,
 		format: "jpg",
 		width: 2480,
 		height: 3508,
+		timeout: 60000,
 	};
-	const converter = pdf2pic.fromBuffer(pdfBuffer, options);
-	await converter.bulk(-1, false);
-	//const pages = await converter.convert();
 
-	const images = [];
+	const convert = fromPath(pdfPath, baseOptions);
 
-	for (let i = 0; i < pages.length; i++) {
-		const filename = `page-${i + 1}.jpg`;
-		const filepath = `${outputFolder}/${filename}`;
-		await fs.writeFile(filepath, pages[i]);
-		images.push(filename);
-	}
+	// fix this shit later in pdf2pic
+	convert.bulk(-1, true).then(async (outputs) => {
+		// Escribir las imágenes en disco y devolver los nombres de archivo
+		const images = [];
+		await asyncForEach(outputs, (output) => {
+			const filename = `base64-${pdfFilename}.${output.page}.jpg`;
+			writeFileSync(outputFolder + "/" + filename, output.base64, "base64");
+			images.push(filename);
+		});
+		// Eliminar el archivo PDF temporal
+		await unlink(pdfPath);
 
-	res.json({ images });
+		// Devolver los nombres de archivo de las imágenes generadas
+		res.json({ images });
+	});
+});
+
+app.get("/imagenes/:nombre", (req, res) => {
+	const nombreImagen = req.params.nombre;
+	const rutaImagen = path.join(__dirname, "output", nombreImagen);
+
+	res.sendFile(rutaImagen);
 });
 
 const port = 3000;
