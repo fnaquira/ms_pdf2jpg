0ef944a4ef0b578744d116d7f4dccd2fdd439c54
Favio Náquira <fnaquiravargas@gmail.com>
2023-11-06 13:19:58 -0500

    [FIX] Ruta inicka

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
 require('dotenv').config();
 
-const express = require("express");
-const bodyParser = require("body-parser");
-const fs = require("fs");
-const path = require("path");
-const { fromPath } = require("pdf2pic");
-const { writeFileSync, unlink } = require("fs-extra");
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+const path = require('path');
+const { fromPath } = require('pdf2pic');
+const { writeFileSync, unlink } = require('fs-extra');
 const logger = require('morgan');
-const asyncForEach = require("asyncforeach_pe");
-const cors = require("cors"); // Importa el middleware cors
+const asyncForEach = require('asyncforeach_pe');
+const cors = require('cors'); // Importa el middleware cors
 
-const multer = require("multer");
-const cron = require("node-cron");
+const multer = require('multer');
+const cron = require('node-cron');
 
 const storage = multer.diskStorage({
 	destination: function (req, file, cb) {
-		cb(null, "./temp/");
+		cb(null, './temp/');
 	},
 	filename: function (req, file, cb) {
-		cb(null, Date.now() + "-" + file.originalname);
-	},
+		cb(null, Date.now() + '-' + file.originalname);
+	}
 });
 
 const upload = multer({
 	storage: storage,
 	fileFilter: function (req, file, cb) {
-		if (!(file.mimetype == "application/pdf" || file.mimetype === "application/octet-stream")) {
-			return cb(new Error("Solo se permiten archivos de PDF"));
+		if (!(file.mimetype == 'application/pdf' || file.mimetype === 'application/octet-stream')) {
+			return cb(new Error('Solo se permiten archivos de PDF'));
 		}
 		cb(null, true);
-	},
+	}
 });
 
 const app = express();
@@ -48,22 +48,26 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // parse application/json
 app.use(bodyParser.json());
 
-app.post("/convertir", upload.single("pdf"), async (req, res) => {
+app.get('/', (req, res) => {
+	res.send('Servicio de conversión de PDF a imágenes');
+});
+
+app.post('/convertir', upload.single('pdf'), async (req, res) => {
 	// Guardar el archivo PDF en la carpeta temporal
 	const pdfFile = req.file;
 	const pdfFilename = pdfFile.filename;
-	const tempFolder = "./temp";
+	const tempFolder = './temp';
 	const pdfPath = path.join(tempFolder, pdfFilename);
 	//return res.json({ message: "Hola mundo" });
 
-	const outputFolder = "./output";
+	const outputFolder = './output';
 	const baseOptions = {
 		density: 100,
 		saveFolder: outputFolder,
-		format: "jpg",
+		format: 'jpg',
 		width: 2480,
 		height: 3508,
-		timeout: 60000,
+		timeout: 60000
 	};
 
 	const convert = fromPath(pdfPath, baseOptions);
@@ -74,7 +78,7 @@ app.post("/convertir", upload.single("pdf"), async (req, res) => {
 		const images = [];
 		await asyncForEach(outputs, (output) => {
 			const filename = `base64-${pdfFilename}.${output.page}.jpg`;
-			writeFileSync(outputFolder + "/" + filename, output.base64, "base64");
+			writeFileSync(outputFolder + '/' + filename, output.base64, 'base64');
 			images.push(filename);
 		});
 		// Eliminar el archivo PDF temporal
@@ -85,27 +89,29 @@ app.post("/convertir", upload.single("pdf"), async (req, res) => {
 	});
 });
 
-app.post("/convertirlos", upload.array("pdfs"), async (req, res) => {
+app.post('/convertirlos', upload.array('pdfs'), async (req, res) => {
 	// Guardar el archivo PDF en la carpeta temporal
 	const pdfFiles = req.files;
 	const pdfFilenames = pdfFiles.map((file) => file.filename);
-	const tempFolder = "./temp";
+	const tempFolder = './temp';
 	const pdfPaths = pdfFilenames.map((filename) => path.join(tempFolder, filename));
 
-	const outputFolder = "./output";
+	const outputFolder = './output';
 	const baseOptions = {
 		density: 100,
 		saveFolder: outputFolder,
-		format: "jpg",
+		format: 'jpg',
 		width: 2480,
 		height: 3508,
-		timeout: 60000,
+		timeout: 60000
 	};
 
 	const convertPromises = pdfPaths.map((pdfPath) => fromPath(pdfPath, baseOptions).bulk(-1, true));
 	const outputsArray = await Promise.all(convertPromises);
 
-	const imagesResponse = pdfFiles.map((file) => {return {name:file.originalname,imgs:[]};});
+	const imagesResponse = pdfFiles.map((file) => {
+		return { name: file.originalname, imgs: [] };
+	});
 
 	// Escribir las imágenes en disco y devolver los nombres de archivo
 	const imagesArray = await Promise.all(
@@ -113,7 +119,7 @@ app.post("/convertirlos", upload.array("pdfs"), async (req, res) => {
 			const images = [];
 			await asyncForEach(outputs, (output) => {
 				const filename = `base64-${pdfFilenames[i]}.${output.page}.jpg`;
-				writeFileSync(outputFolder + "/" + filename, output.base64, "base64");
+				writeFileSync(outputFolder + '/' + filename, output.base64, 'base64');
 				//images.push(filename);
 				imagesResponse[i].imgs.push(filename);
 			});
@@ -128,9 +134,9 @@ app.post("/convertirlos", upload.array("pdfs"), async (req, res) => {
 	res.json(imagesResponse);
 });
 
-app.get("/imagenes/:nombre", (req, res) => {
+app.get('/imagenes/:nombre', (req, res) => {
 	const nombreImagen = req.params.nombre;
-	const rutaImagen = path.join(__dirname, "output", nombreImagen);
+	const rutaImagen = path.join(__dirname, 'output', nombreImagen);
 
 	res.sendFile(rutaImagen);
 });
@@ -141,17 +147,17 @@ app.listen(port, () => {
 	console.log(`Example app listening on port ${port}`);
 });
 
-const folderPath = "output";
+const folderPath = 'output';
 
-cron.schedule("31 * * * *", () => {
-	console.log("Deleting files");
+cron.schedule('31 * * * *', () => {
+	console.log('Deleting files');
 	// Obtener la fecha actual y restarle 3 horas
 	const currentDateTime = new Date();
 	const threeHoursAgo = new Date(currentDateTime.getTime() - 3 * 60 * 60 * 1000);
 	// Obtener la lista de archivos en la carpeta
 	fs.readdirSync(folderPath).forEach(async (file) => {
 		// Ignorar el archivo .gitignore
-		if (file === ".gitignore") {
+		if (file === '.gitignore') {
 			return;
 		}
 
